758508a94ad5dc43da1e1bb8d7f14f1170798588
fatma-bouraoui <88318777+fatma-bouraoui@users.noreply.github.com>
2021-10-07 10:35:01 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Head from './Components/Head';
+import Todolist from './Components/Todolist';
+
+function App() {
+  return (
+    <div className="App">
+      <Head />
+      <Todolist />
+    </div>
+  );
+}
+
+export default App;
